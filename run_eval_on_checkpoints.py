a32ffedeaca7250f9829a7288796817d2fae3de9
tomyoung <tomyoung903@gmail.com>
2024-01-12 03:22:12 +0800

    llm harness + billama

---
+++ run_eval_on_checkpoints.py
@@ -0,0 +1,21 @@
+import os
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("python main.py --model hf-causal --model_args pretrained=\"/scratch/users/nus/tomyoung/Colossal-llama2/ColossalAI/examples/language/llama2/checkpoint/epoch0-step22000model\" --tasks arc_challenge --num_fewshot=25 --output_path /scratch/users/nus/tomyoung/llm_eval/lm-evaluation-harness/arc_challenge_epoch0-step22000.json")
